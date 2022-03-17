f68f05a5bf0627053f745c7dcfc77a7d20f4c671
Bnuuy <83287502+HideOnFluff@users.noreply.github.com>
2022-03-16 22:55:37 -0300

    Modified gitignore

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com:HideOnFluff/HideOnFluff.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:HideOnFluff/VueJs---Second-Iteration.git origin:gh-pages
+#git push -f git@github.com:HideOnFluff/VueJs---Second-Iteration.git origin:gh-pages
 
 cd -
