1c08f1736857881067081c60b88a1258ef9e9680
Bnuuy <83287502+HideOnFluff@users.noreply.github.com>
2022-03-16 21:53:28 -0300

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 // vue.config.js file to be placed in the root of your repository
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/VueJs---Second-Iteration/'
-      : '/'
+    publicPath: process.env.NODE_ENV === '/VueJs---Second-Iteration/' ? '/VueJs---Second-Iteration/' : '/',
   }
